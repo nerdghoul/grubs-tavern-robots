a21af31fdfb9779d4176bff09c0700275b7cbd7f
toomanybooks <benjtibbetts@gmail.com>
2020-08-02 02:23:24 -0400

    add bot as example/to try deploying somewhere

---
+++ scurvybot/bot.py
@@ -0,0 +1,23 @@
+import os
+import random
+
+import discord
+from dotenv import load_dotenv
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == 'argh':
+        response = random.choice([line.rstrip('\n') for line in open('yohoho.txt')])
+        await message.channel.send(response)
+
+client.run(TOKEN)
